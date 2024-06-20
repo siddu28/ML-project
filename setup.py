99a2141d399c0689741b12656e0dc0bd301f1405
siddu28 <siddukasam28@gmail.com>
2024-06-20 21:45:08 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -13,9 +13,8 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPHEN_E_DOT in requirements:
+        if HYPHEN_E_DOT in requirements:    
             requirements.remove(HYPHEN_E_DOT)
-    
     return requirements
 
 
